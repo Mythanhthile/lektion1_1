47042314a60fd0944ae0a169a93d09ca6c6df93c
Mythanhthile <93250566+Mythanhthile@users.noreply.github.com>
2023-08-25 13:20:59 +0200

    exercise4

--- projects/models/src/public-api.ts
+++ projects/models/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/models.service';
 export * from './lib/models.component';
 export * from './lib/models.module';
+export * from './lib/course';
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+import { Course } from 'models';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'lektion1_1';
+  courses: Course[] = [
+    {
+      name: 'Advanced Frontend Development',
+      code: 'SWAFE',
+      ects: 5,
+    },
+    {
+      name: 'Mobile Application Development',
+      code: 'SWMAD-01',
+      ects: 5,
+    },
+  ];
 }
